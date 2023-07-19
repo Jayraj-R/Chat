f68508738911ac54b317bd21e1f194f0a5537cd1
Jayraj Rathod <jayrajrathodph@gmail.com>
2023-07-19 12:37:35 +0530

    chore: Initial setup and layout with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello there!
-    </div>
-  );
+	return (
+		<div className='App container mx-auto flex-auto'>
+			<section
+				className='navbar_container container mx-auto'
+				style={{ background: 'pink' }}
+			>
+				Navbar
+			</section>
+			<div className='body__container container mx-auto flex flex-row h-screen'>
+				<section
+					className='left__pane__container container'
+					style={{ background: 'blue' }}
+				>
+					Left pane
+				</section>
+				<section
+					className='right__pane__container container'
+					style={{ background: 'green' }}
+				>
+					Right pane
+				</section>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		colors: {
+			white: '#FBFDFF',
+			black: '#14181D',
+			black: '#14191D',
+			'blue-light': '#C1D2DF',
+			gray: '#a6acb1',
+			'gray-light': '#C3CED3',
+			'gray-dark': '#3d4347',
+			'brown-golden': '#cb8000',
+			'brown-light': '#ffb544',
+		},
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
